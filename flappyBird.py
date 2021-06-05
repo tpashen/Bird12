553210ce48cbfb79c0a0269f48ad4f89bb65899e
tpashen <81863630+tpashen@users.noreply.github.com>
2021-06-05 13:49:23 +0300

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -33,6 +33,15 @@ class Area():
     def draw(self, shift_x=0, shift_y=0):
         self.fill()
         mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+class Picture(Area):
+    def __init__(self, filename, x = 0, y = 0, width = 10, height = 10, angle = 0):
+        Area.__init__(self, x=x, y=y, width=width, height=height, color=None)
+        self.image = pygame.image.load(filename)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.image = pygame.transform.rotate(self.image, angle)
+        
+    def draw(self):
+        mw.blit(self.image, (self.rect.x, self.rect.y))
 bird = Picture('bird.png', 160, 200, 60, 40)
 columnMove = 400
 move_up = False
